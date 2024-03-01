6458a73013b8bc7defb3a9751026710f3d863269
wnsdud1302 <wnsdud1302@naver.com>
2024-03-01 17:39:25 +0900

    first commit

--- app/api/project/image/route.ts
+++
@@ -1,8 +0,0 @@
-import { readdir } from "fs/promises";
-import { NextRequest, NextResponse } from "next/server";
-
-
-export async function GET(req: NextRequest) {
-    const files = await readdir("public/images");
-    return NextResponse.json(files);
-}
