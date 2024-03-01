6751e99d62c94ee49bb104aa71d577152eadb99b
wnsdud1302 <wnsdud1302@naver.com>
2024-03-02 02:36:35 +0900

    first commit

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const backendServer = '172.15.0.3:8080'
-export const FrontendServer = dev ? 'http://localhost:3000' : '172.15.0.3'
+export const backendServer = '172.15.0.1:8080'
+export const FrontendServer = dev ? 'http://localhost:3000' : '172.15.0.1:3000'
