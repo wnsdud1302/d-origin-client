d1aa2e21e66f41067d3acd17998327bae3e4b560
wnsdud1302 <wnsdud1302@naver.com>
2024-03-02 15:50:37 +0900

    first commit

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const backendServer = '172.15.0.3:8080'
+export const backendServer = dev? 'd-origin.synology.me:8080' : '172.15.0.3:8080'
 export const FrontendServer = dev ? 'http://localhost:3000' : '172.15.0.4:3000'
