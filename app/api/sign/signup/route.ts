00bbcec3149deba9076e646b6967db0888c3fba8
wnsdud1302 <wnsdud1302@naver.com>
2024-03-03 19:28:51 +0900

    나머지 구현

--- app/api/sign/signup/route.ts
+++ app/api/sign/signup/route.ts
@@ -9,36 +9,18 @@ interface RequsetBody {
     password: string
 }
 
-// export async function POST(req: NextRequest){
-//     const body: RequsetBody = await req.json()
-
-//     const user = await prisma.user.create({
-//         data:{
-//             name: body.name,
-//             email: body.email,
-//             password: await bcrypt.hash(body.password, 12),
-//         },
-//     })
-
-//     const { password, ...result} = user
-//     return NextResponse.json(result)
-// }
-
 export async function POST(req: NextRequest){
+    const body: RequsetBody = await req.json()
 
-    const body = await req.json()
-
-    try{
-    const res = await fetch(`/api/sign/login`, {
-        method: 'POST',
-        body: body,
-        headers: { "Content-Type": "application/json" }
+    const user = await prisma.user.create({
+        data:{
+            name: body.name,
+            email: body.email,
+            password: await bcrypt.hash(body.password, 12),
+        },
     })
-    const user = await res.json()
 
-    return NextResponse.json({user})
-    } catch (error){
-        return NextResponse.error()
-    }
-
+    const { password, ...result} = user
+    return NextResponse.json(result)
 }
+
