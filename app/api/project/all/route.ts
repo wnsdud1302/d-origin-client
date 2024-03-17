3dcc598f8c07e8790fc76fc8ba8e4e52944f2e42
wnsdud1302 <wnsdud1302@naver.com>
2024-03-18 02:59:54 +0900

    version0.0.2

--- app/api/project/all/route.ts
+++ app/api/project/all/route.ts
@@ -3,12 +3,16 @@ import { backendServer } from "../../../config";
 
 
 export async function GET(req: NextRequest) {
-    
+
+    try {
         const res = await fetch(`${backendServer}/project/list`, {
             cache: "no-store",
         })
         console.log(res.status)
         const data = await res.json()
         return NextResponse.json(data)
+    } catch (e) {
+        return NextResponse.json({ error: e.message, status: 500 })
+    }
 
 }
