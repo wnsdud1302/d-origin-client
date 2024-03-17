387480071416cc605326d307c5d10213101e50ba
wnsdud1302 <wnsdud1302@naver.com>
2024-03-18 03:15:15 +0900

    수정추가

--- app/api/project/list/route.ts
+++ app/api/project/list/route.ts
@@ -2,17 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { backendServer } from "../../../config";
 
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest){
 
-    try {
-        const res = await fetch(`${backendServer}/project/list`, {
-            cache: "no-store",
-        })
-        console.log(res.status)
-        const data = await res.json()
-        return NextResponse.json(data)
-    } catch (e) {
-        return NextResponse.json({ error: e.message, status: 500 })
-    }
+    const query = req.nextUrl.searchParams
+    const res = await fetch(`${backendServer}/project/list`, {
+        cache: 'no-store'
+    })
+    const data = await res.json()
+    return NextResponse.json(data)
 
 }
