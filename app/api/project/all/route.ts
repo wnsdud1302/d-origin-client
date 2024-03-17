9dc2fdee345761b16e363a029616a13ec50e331a
wnsdud1302 <wnsdud1302@naver.com>
2024-03-18 01:51:35 +0900

    수정추가

--- app/api/project/all/route.ts
+++ app/api/project/all/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { backendServer } from "../../../config";
 
 
-export async function GET(req: NextRequest){
-    try{ 
-        const res = await fetch(`${backendServer}/project?page=0&size=1000`, {
-            method: 'GET',
-            cache: 'no-store',
+export async function GET(req: NextRequest) {
+    try{
+        const res = await fetch(`${backendServer}/project/all`, {
+            cache: "no-store",
         })
         const data = await res.json()
         return NextResponse.json(data)
-    } catch(error){
-        return NextResponse.json({error: error.message, status: 500})
+    } catch(e){
+        return NextResponse.json({error: e.message, status: 500})
     }
+
 }
