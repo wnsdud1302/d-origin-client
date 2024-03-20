83e93a6189b222aacf634d822ce4c19c25c0931f
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2024-03-20 10:22:39 +0900

    News post grid 오타 수정

--- app/post/news/page.tsx
+++ app/post/news/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Page(){
     return(
-        <div className="gird grid-cols-1 text-center gap-8 mt-[100px]">
+        <div className="grid grid-cols-1 text-center gap-8 mt-[100px]">
             <Link href={'news/create'}>뉴스 만들기</Link>
             <Link href={'news/modify'}>뉴스 수정하기</Link>
         </div>
