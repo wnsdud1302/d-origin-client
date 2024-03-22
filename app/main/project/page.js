8ddce7092837ac294c593f65bde7e02132bec381
wnsdud1302 <wnsdud1302@naver.com>
2024-03-23 02:40:15 +0900

    이미지 로더 테스트

--- app/main/project/page.js
+++ app/main/project/page.js
@@ -45,7 +45,7 @@ export default function Page() {
       return (
         <div key={index}>
           <Link className=' overflow-hidden' href={`project/${name}`}>
-            <Image className='newsImage h-[auto] max-w-[100%] overflow-hidden' src={`/images/${name}/1.jpeg`} alt={element.name} width={500} height={300}/>
+            <img className='newsImage h-[auto] max-w-[100%] overflow-hidden' src={`/images/${name}/1.jpeg`} alt={element.name} width={500} height={300}/>
             <p className=' text-center mt-2'>{element.name}</p>
           </Link>
         </div>
