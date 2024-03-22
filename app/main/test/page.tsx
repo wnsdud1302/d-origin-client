6ab3a677dd856fd37395131c6e1c8bfe35adbd9c
wnsdud1302 <wnsdud1302@naver.com>
2024-03-23 03:26:54 +0900

    이미지 api 생성

--- app/main/test/page.tsx
+++ app/main/test/page.tsx
@@ -1,7 +1,7 @@
 import { NextRequest } from 'next/server'
 import { frontendServer } from '../../config'
 
-export async function Page(req: NextRequest){
+export default async function Page(req: NextRequest){
     const name = req.nextUrl.searchParams.get('name')
     const res = await fetch(`${frontendServer}/api/project/image?name=${name}`)
 
