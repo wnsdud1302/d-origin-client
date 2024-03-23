7b6cbca6d7008fd9e652601f07e9c295e5d40acd
wnsdud1302 <wnsdud1302@naver.com>
2024-03-23 16:41:39 +0900

    이미지 api 테스트

--- app/config.js
+++ app/config.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 export const backendServer = dev ? 'http://d-origin.kr:8080' : 'http://172.15.0.3:8080'
 export const frontendServer = dev ? 'http://localhost:3000' : 'd-origin.kr'
+// export const frontendServer = dev ? 'http://localhost:3000' : 'http://localhost:3000'
--- app/main/test/[name]/page.tsx
+++ app/main/test/[name]/page.tsx
@@ -3,7 +3,6 @@ import  Image  from 'next/image'
 
 export default async function Page({params}){
     const name = decodeURIComponent(params.name)
-
     return(
         <div>
             <Image src={`${frontendServer}/api/project/image?name=${name}`} alt={name} width={2000} height={300}/>
