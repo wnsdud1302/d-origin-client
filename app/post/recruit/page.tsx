7a98aa82c327e823e0b89e5c1e79bd33858a1ebc
wnsdud1302 <wnsdud1302@naver.com>
2024-03-25 01:35:40 +0900

    recruit 수정

--- app/post/recruit/page.tsx
+++ app/post/recruit/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Page(){
     return(
         <div className="grid grid-cols-1 text-center gap-8 mt-[100px] text-xl">
-            <Link href='/post/recruit/modify'>구인 등록</Link>
-            <Link href='/post/recruit/register'>구인 수정</Link>
+            <Link href='/post/recruit/create'>구인 등록</Link>
+            <Link href='/post/recruit/modify'>구인 수정</Link>
 
         </div>
     )
