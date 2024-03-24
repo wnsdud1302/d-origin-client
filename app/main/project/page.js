3db515bfec9ff6832d489df07aca068abd92787f
wnsdud1302 <wnsdud1302@naver.com>
2024-03-24 22:33:34 +0900

    config 수정

--- app/main/project/page.js
+++ app/main/project/page.js
@@ -42,12 +42,11 @@ export default function Page() {
   const ProejctList = ({ data }) => {
     
     const grid = data.map((element, index) => {
-      const name = decodeURIComponent(element.name)
-      console.log(name)
+      const name = element.name
       return (
         <div key={index}>
           <Link className='' href={`project/${name}`}>
-            <Image unoptimized className='flex items-center justify-center object-cover h-[200px] w-[80%] mx-auto' 
+            <Image className='flex items-center justify-center object-cover h-[200px] w-[80%] mx-auto' 
              src={`${frontendServer}/api/project/image?name=${name}`} alt={element.name} width={1000} height={300}/>
             <p className='text-center mt-2'>{element.name}</p>
           </Link>
