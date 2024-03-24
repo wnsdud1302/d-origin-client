ff110df1d426e5b4a0abf7401ce7906dc3860a40
wnsdud1302 <wnsdud1302@naver.com>
2024-03-24 22:41:26 +0900

    이미지 test

--- app/main/test/[name]/page.tsx
+++ app/main/test/[name]/page.tsx
@@ -1,11 +1,17 @@
 import { frontendServer } from '../../../config'
 import  Image  from 'next/image'
 
+
+const loader = ({src, width, quality}) => {
+    return `${src}?w=${width}&q=${quality || 75}`
+}
+
 export default async function Page({params}){
     const name = decodeURIComponent(params.name)
     return(
         <div>
-            <Image src={`${frontendServer}/api/project/image?name=${name}`} alt={name} width={2000} height={300}/>
+            <Image loader={loader} src={`${frontendServer}/api/project/image?name=${name}`} alt={name} width={2000} height={300}/>
+            <img src={`${frontendServer}/api/project/image?name=${name}`} alt={name} width={2000} height={300}/>
         </div>
     )
 }
