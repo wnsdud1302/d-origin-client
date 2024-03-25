faa101b4787f707f2600c062b19e398710cd3a7d
wnsdud1302 <wnsdud1302@naver.com>
2024-03-25 18:05:14 +0900

    최종본

--- app/main/project/[name]/page.js
+++ app/main/project/[name]/page.js
@@ -1,13 +1,13 @@
+'use client'
 import { frontendServer, backendServer } from '../../../config';
 import Image from 'next/image';
+
+const fetcher = async (url) => fetch(url).then(res => res.json())
 
 export default async function page({ params }) {
     const decode = decodeURIComponent(params.name);
 
-    const res = await fetch(`${backendServer}/project/${decode}`, {
-        cache: 'no-store'
-    })
-    const project = await res.json()
+    const {data: project, error} = useSWR(`/api/project/modify=${decode}`, fetcher)
 
     return (
         <div className='fade-in-up mx-auto mt-[50px]'>
@@ -17,10 +17,10 @@ export default async function page({ params }) {
                    src={`${frontendServer}/api/project/image?name=${project.name}`} 
                    onContextMenu={e=>e.preventDefault()} 
                    width={1000} height={300}/>
-            <div className=' grid grid-cols-2 mt-[20px]'>
+            {project && <div className=' grid grid-cols-2 mt-[20px]'>
                 <DescribeTable project={project}/>
                 <p className='mt-[0px] text-[15px] whitespace-pre-wrap'>{project.description}</p>
-            </div>
+            </div>}
             
         </div>
     );
