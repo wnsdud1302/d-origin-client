a21da4fd82b619785cef49a10c5ce6e25e233bcf
wnsdud1302 <wnsdud1302@naver.com>
2024-03-25 14:28:52 +0900

    recruit 수정

--- app/api/recruit/list/route.ts
+++ app/api/recruit/list/route.ts
@@ -2,13 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { backendServer } from "../../../config";
 
 export async function GET(req: NextRequest){
-    try{
         const res = await fetch(`${backendServer}/recruit/list`, {
             cache: "no-store"
         })
         const data = await res.json()
         return NextResponse.json(data)
-    } catch(e){
-        return NextResponse.json({error: e.message, status: 500})
-    }
+
 }
