1351835d8b926f273f1e64b37233dff89cdd9163
wnsdud1302 <wnsdud1302@naver.com>
2024-03-26 11:28:15 +0900

    이미지 다운로드 제거

--- app/main/news/[title]/page.js
+++ app/main/news/[title]/page.js
@@ -5,27 +5,23 @@ import useSWR from 'swr'
 
 const fetcher = async (url) => fetch(url).then(res => res.json())
 
-export default async function Page({params}) {
+export default function Page({params}) {
     const decode = decodeURIComponent(params.title)
 
-    const {data, error} = useSWR(`${backendServer}/news/${decode}`, fetcher)
-
-    const res = await fetch(`${backendServer}/news/${decode}` , {
-        cache: "no-store",
-    })
-    const news = await res.json()
+    const {data: news, error} = useSWR(`/api/news/modify?title=${decode}`, fetcher)
 
     return (
         <div>
-            <div className='bg-white w-[60px] h-[5px] mx-auto mb-[10px] relative mt-[40px]'/>
-            <h1 className='newsTitle mb-[10px]'>{news.title}</h1>
-            <div className='mx-auto w-[95vw] whitespace-pre-wrap'>
-                <Image src={`${frontendServer}/api/news/image?title=${news.title}`} 
-                       alt={news.title} 
-                       width={2000} height={300}/>
-                <p className='mt-[10px]'>{news.content}</p>
+            {news &&
+            <><div className='bg-white w-[60px] h-[5px] mx-auto mb-[10px] relative mt-[40px]' /><h1 className='newsTitle mb-[10px]'>{news.title}</h1><div className='mx-auto w-[95vw] whitespace-pre-wrap'>
+                    <Image src={`${frontendServer}/api/news/image?title=${news.title}`}
+                        onContextMenu={e => e.preventDefault()}
+                        alt={news.title}
+                        width={2000} height={300} />
+                    <p className='mt-[10px]'>{news.content}</p>
 
-            </div>
+                </div></>
+            }
             
         </div>
     );
--- app/main/project/[name]/page.js
+++ app/main/project/[name]/page.js
@@ -7,23 +7,21 @@ const fetcher = async (url) => fetch(url).then(res => res.json())
 
 export default function Page({ params }) {
     const decode = decodeURIComponent(params.name);
-
-    const {data: project, error} = useSWR(`${backendServer}/project/${decode}`, fetcher)
+    console.log(decode)
+    const {data: project, error} = useSWR(`/api/project/modify?name=${decode}`, fetcher)
 
     return (
         <div className='fade-in-up mx-auto mt-[50px]'>
-            <div className=' bg-white w-[60px] h-[5px] mx-auto mb-10px'></div>
-            <h1 className=' text-center text-[40px]'>{project.name}</h1>
-            <Image className='flex items-center justify-center mx-auto'
-                   alt={project.name}
-                   src={`${frontendServer}/api/project/image?name=${project.name}`} 
-                   onContextMenu={e=>e.preventDefault()} 
-                   width={1000} height={300}/>
-            {project && <div className=' grid grid-cols-2 mt-[20px]'>
-                <DescribeTable project={project}/>
-                <p className='mt-[0px] text-[15px] whitespace-pre-wrap'>{project.description}</p>
-            </div>}
-            
+            { project &&
+            <><div className=' bg-white w-[60px] h-[5px] mx-auto mb-10px'></div><h1 className=' text-center text-[40px]'>{project.name}</h1><Image className='flex items-center justify-center mx-auto'
+                    alt={project.name}
+                    src={`${frontendServer}/api/project/image?name=${project.name}`}
+                    onContextMenu={e => e.preventDefault()}
+                    width={1000} height={300} /><div className=' grid grid-cols-2 mt-[20px]'>
+                        <DescribeTable project={project} />
+                        <p className='mt-[0px] text-[15px] whitespace-pre-wrap'>{project.description}</p>
+            </div></>
+            }
         </div>
     );
 }
