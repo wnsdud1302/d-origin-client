2ebfe395854631492b261ac7997ddb7c086f505c
wnsdud1302 <wnsdud1302@naver.com>
2024-05-08 15:44:02 +0900

    https로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
                 hostname: 'localhost',
             },
             {
-                protocol: 'http',
+                protocol: 'https',
                 hostname: 'd-origin.kr',
                 port: '',
                 pathname: '/**'
